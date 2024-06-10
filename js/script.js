d116ac614c7e4f1b51ab35b43e70ed4e94b68cdb
jcsoldevilla1256 <soldevillajohncarlos@gmail.com>
2024-06-10 20:49:17 +0800

    Add function for every click event in card will flip

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+// declaring a constant and selecting all card elements 
 const cards = document.querySelectorAll(".card");
+
+function flipCard(e) {
+    let clickedCard = e.target; //getting user clicked card
+    clickedCard.classList.add("flip"); //add flip class in every card clicked
+};
+
+// add click event to all card element 
+cards.forEach(card => {
+    card.addEventListener("click", flipCard);
+});

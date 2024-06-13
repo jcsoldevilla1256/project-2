40dd39df747633469d72aeb4bde50a42924245a3
jcsoldevilla1256 <soldevillajohncarlos@gmail.com>
2024-06-13 12:42:50 +0800

    Program card shuffle

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // declaring a constant and selecting all card elements 
 const cards = document.querySelectorAll(".card");
 
+let matchedCard = 0;
 let cardOne, cardTwo; //declare cardOne and cardTwo
 let disableDeck = false;
 
@@ -22,6 +23,12 @@ function flipCard(e) {
 
 function matchCards(img1, img2) {
     if(img1 === img2) {
+        matchedCard++; // increment matched value by 1
+        if (matchedCard == 8){
+            setTimeout ( () => {
+                return shuffleCard();
+            }, 1000); // calling shuffle card function after 1s
+        };
         cardOne.removeEventListener("click", flipCard);
         cardTwo.removeEventListener("click", flipCard);
         cardOne = cardTwo = ""; //setting both card value to blank
@@ -44,7 +51,28 @@ function matchCards(img1, img2) {
     }, 1200);
 }
 
+function shuffleCard() {
+    matchedCard = 0;
+    cardOne = cardTwo = "";
+    disableDeck = false;
+    // creating array of 16 items and each item are repeated twice
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1); // sorting array item randomly
+    
+    // removing flip class from all cards and passing random image to each card
+    cards.forEach((card, index) => {
+        card.classList.remove("flip");
+        let imgTag = card.querySelector("img");
+        imgTag.src = `./img/poke-${arr[index]}.png`;
+        card.addEventListener("click", flipCard);
+    });
+}
+
+shuffleCard();
+
 // add click event to all card element 
 cards.forEach(card => {
     card.addEventListener("click", flipCard);
 });
+
+

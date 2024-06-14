c020f559073a68e9e33626876dcc55d0d95798f4
jcsoldevilla1256 <soldevillajohncarlos@gmail.com>
2024-06-14 10:02:02 +0800

    Format document

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// declaring a constant and selecting all card elements 
+// declaring a constant and selecting all card elements
 const cards = document.querySelectorAll(".card");
 
 let matchedCard = 0;
@@ -6,92 +6,94 @@ let cardOne, cardTwo; //declare cardOne and cardTwo
 let disableDeck = false;
 let matchAttempt = 16;
 
-
 function flipCard(e) {
-    let clickedCard = e.target; //getting user clicked card
-    if (clickedCard !== cardOne && !disableDeck) {
-        clickedCard.classList.add("flip"); //add flip class in every card clicked
-    if(!cardOne) { // prevent to click cardOne again
-        // return cardOne value to clickedCard
-        return cardOne = clickedCard;
+  let clickedCard = e.target; //getting user clicked card
+  if (clickedCard !== cardOne && !disableDeck) {
+    clickedCard.classList.add("flip"); //add flip class in every card clicked
+    if (!cardOne) {
+      // prevent to click cardOne again
+      // return cardOne value to clickedCard
+      return (cardOne = clickedCard);
     }
 
     cardTwo = clickedCard;
     disableDeck = true;
     let cardOneImg = cardOne.querySelector("Img").src,
-    cardTwoImg = cardTwo.querySelector("Img").src;
+      cardTwoImg = cardTwo.querySelector("Img").src;
     matchCards(cardOneImg, cardTwoImg);
-    };
-};
+  }
+}
 
 function matchCards(img1, img2) {
-    if(img1 === img2) {
-        matchedCard++; // increment matched value by 1
-        if (matchedCard == 8){
-            winModal(); // calling shuffle card function after 1s
-        };
-        cardOne.removeEventListener("click", flipCard);
-        cardTwo.removeEventListener("click", flipCard);
-        cardOne = cardTwo = ""; //setting both card value to blank
-        return disableDeck = false;
+  if (img1 === img2) {
+    matchedCard++; // increment matched value by 1
+    if (matchedCard == 8) {
+      winModal(); // calling shuffle card function after 1s
     }
+    cardOne.removeEventListener("click", flipCard);
+    cardTwo.removeEventListener("click", flipCard);
+    cardOne = cardTwo = ""; //setting both card value to blank
+    return (disableDeck = false);
+  }
 
-    // set attempt
-    matchAttempt --;
-    attemptsLeft();
+  // set attempt
+  matchAttempt--;
+  attemptsLeft();
 
-    //test create modal
-    if (matchAttempt == 0){
-        loseModal();
-    }
+  //test create modal
+  if (matchAttempt == 0) {
+    loseModal();
+  }
 
-    // if two cards not matched
-    setTimeout ( () => {
-        // add shake class after 400ms
-        cardOne.classList.add("shake");
-        cardTwo.classList.add("shake");
-    }, 400);
+  // if two cards not matched
+  setTimeout(() => {
+    // add shake class after 400ms
+    cardOne.classList.add("shake");
+    cardTwo.classList.add("shake");
+  }, 400);
 
-    setTimeout ( () => {
-        // remove shake and flip card after 1200ms
-        cardOne.classList.remove("shake", "flip");
-        cardTwo.classList.remove("shake", "flip");
-        cardOne = cardTwo = ""; //setting both card value to blank
-        disableDeck = false;
-    }, 1200);
+  setTimeout(() => {
+    // remove shake and flip card after 1200ms
+    cardOne.classList.remove("shake", "flip");
+    cardTwo.classList.remove("shake", "flip");
+    cardOne = cardTwo = ""; //setting both card value to blank
+    disableDeck = false;
+  }, 1200);
 }
 
 function shuffleCard() {
-    console.log("shuffled");
-    matchAttempt = 16;
-    attemptsLeft();
-    matchedCard = 0;
-    cardOne = cardTwo = "";
-    disableDeck = false;
-    // creating array of 16 items and each item are repeated twice
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-    arr.sort(() => Math.random() > 0.5 ? 1 : -1); // sorting array item randomly
-    
-    // removing flip class from all cards and passing random image to each card
-    cards.forEach((card, index) => {
-        card.classList.remove("flip");
-        let imgTag = card.querySelector("img");
-        imgTag.src = `./img/poke-${arr[index]}.png`;
-        card.addEventListener("click", flipCard);
-    });
+  console.log("shuffled");
+  matchAttempt = 16;
+  attemptsLeft();
+  matchedCard = 0;
+  cardOne = cardTwo = "";
+  disableDeck = false;
+  // creating array of 16 items and each item are repeated twice
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+  arr.sort(() => (Math.random() > 0.5 ? 1 : -1)); // sorting array item randomly
+
+  // removing flip class from all cards and passing random image to each card
+  cards.forEach((card, index) => {
+    card.classList.remove("flip");
+    let imgTag = card.querySelector("img");
+    imgTag.src = `./img/poke-${arr[index]}.png`;
+    card.addEventListener("click", flipCard);
+  });
 }
 
 shuffleCard(); //call shuffle
 attemptsLeft(); //call attempt
 
 // attempt function
-function attemptsLeft(){
-    document.querySelector(".attempts").innerHTML = matchAttempt;
-};
+function attemptsLeft() {
+  document.querySelector(".attempts").innerHTML = matchAttempt;
+}
 
 // modal function
 function winModal() {
-    document.querySelector(".modal").innerHTML += `<div id="modalContainer" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+  document.querySelector(
+    ".modal"
+  ).innerHTML += `<div id="modalContainer" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
   <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
   
   <div class="fixed inset-0 z-10 flex items-center justify-center p-4 overflow-y-auto">
@@ -118,7 +120,9 @@ function winModal() {
 }
 
 function loseModal() {
-    document.querySelector(".modal").innerHTML += `<div id="modalContainer" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+  document.querySelector(
+    ".modal"
+  ).innerHTML += `<div id="modalContainer" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
   <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
   
   <div class="fixed inset-0 z-10 flex items-center justify-center p-4 overflow-y-auto">
@@ -145,16 +149,14 @@ function loseModal() {
 }
 
 function resetButton() {
-    document.querySelector(".modal").innerHTML = "";
-    setTimeout ( () => {
-        return shuffleCard();
-    }, 700);
-};
+  document.querySelector(".modal").innerHTML = "";
+  setTimeout(() => {
+    return shuffleCard();
+  }, 700);
+}
 
-// add click event to all card element 
-cards.forEach(card => {
-    // card.classList.add("flip")
-    card.addEventListener("click", flipCard);
+// add click event to all card element
+cards.forEach((card) => {
+  // card.classList.add("flip")
+  card.addEventListener("click", flipCard);
 });
-
-
